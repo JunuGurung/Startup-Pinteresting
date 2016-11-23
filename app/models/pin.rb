2e25e0c27816c3cc36278fa41b06f202ba4a893c
Junu Gurung <junugurung48@gmail.com>
2016-11-23 18:07:24 -0500

    bootstrap viewport

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,8 @@
 class Pin < ApplicationRecord
+	belongs_to :user
+
+	has_attached_file :image, :style =>{:medium => "300*300>", :thumb => "100*100>"}, :default_url =>"/images/:style/missing.png"
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113104935) do
+ActiveRecord::Schema.define(version: 20161114025820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.index ["user_id"], name: "index_pins_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
